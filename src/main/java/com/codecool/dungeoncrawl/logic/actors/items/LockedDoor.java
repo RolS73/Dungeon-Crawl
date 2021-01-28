e6cb83ba67e2d13fb637928e5cb918a4dfd6c285
Gergő Csongor Vincze <65530422+Asu-Tia@users.noreply.github.com>
2021-01-28 11:12:28 +0100

    Locked Door opens and Key removed

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/items/LockedDoor.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/items/LockedDoor.java
@@ -20,7 +20,13 @@ public class LockedDoor extends Item implements InteractiveObject {
 
         @Override
         public boolean isThisObjectInteractive(){
-            return Main.Inventory.contains("Key of Wisdom");
+            for (Item item : Main.Inventory) {
+                if (item.getName().equals("Key of Wisdom")) {
+                    return true;
+                }
+            }
+            return false;
+//            return Main.Inventory.contains("Key of Wisdom");
         }
 
         @Override
@@ -28,7 +34,8 @@ public class LockedDoor extends Item implements InteractiveObject {
             if (isThisObjectInteractive()) {
                 this.getCell().setItem(new OpenedDoor(getCell()));
                 this.getCell().setCellType(CellType.FLOOR);
-                Main.Inventory.remove("Key of Wisdom");
+                Main.Inventory.removeIf(item -> item.getName().equals("Key of Wisdom"));
+//                Main.Inventory.remove("Key of Wisdom");
             }
         }
 
