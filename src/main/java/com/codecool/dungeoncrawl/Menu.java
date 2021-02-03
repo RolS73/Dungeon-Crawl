576d6d6218499bc058571c272f3fef5b45c6fc21
Gergő Csongor Vincze <65530422+Asu-Tia@users.noreply.github.com>
2021-02-03 15:01:20 +0100

    Add Textfield

--- src/main/java/com/codecool/dungeoncrawl/Menu.java
+++ src/main/java/com/codecool/dungeoncrawl/Menu.java
@@ -4,6 +4,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -12,7 +13,7 @@ import javafx.scene.layout.VBox;
 public class Menu {
 
     private final BorderPane menuLayout = new BorderPane();
-    private final Scene menuScreen = new Scene(menuLayout, 500, 500);
+    private final Scene menuScreen = new Scene(menuLayout, 300, 300);
 
     public Scene getMenuScreen() {
         return menuScreen;
@@ -24,14 +25,30 @@ public class Menu {
         HBox titledPane = new HBox();
         titledPane.setAlignment(Pos.CENTER);
         titledPane.getChildren().add(title);
-
         menuLayout.setTop(titledPane);
 
         VBox menu = new VBox();
         menu.setAlignment(Pos.CENTER);
+
+        Label askForName = new Label("Please enter your name!");
+
+        TextField playerName = new TextField();
+        playerName.setMaxWidth(100);
+
         Button playButton = new Button("Play!");
+
+        playButton.setDisable(true);
+
         playButton.setOnAction(play -> Main.stage.setScene(Main.gameScene));
-        menu.getChildren().add(playButton);
+        playerName.textProperty().addListener((observable, oldvalue, newvalue) -> {
+            if (newvalue.equals("")) {
+                playButton.setDisable(true);
+            } else {
+                playButton.setDisable(false);
+            }
+        });
+
+        menu.getChildren().addAll(askForName, playerName, playButton);
         menuLayout.setCenter(menu);
     }
 
