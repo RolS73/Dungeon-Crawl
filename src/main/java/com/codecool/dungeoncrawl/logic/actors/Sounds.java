39f66bb10fdb5ab51f98aa0e2ef6b3de4694a84f
deszus <68805470+deszus@users.noreply.github.com>
2021-02-12 11:33:03 +0100

    BGsound, EVsound

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Sounds.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Sounds.java
@@ -6,20 +6,65 @@ import java.io.IOException;
 import java.net.URL;
 
 public class Sounds {
+    private static Clip bgMusic;
+    private static Clip eventMusic ;
+
     public static void playSound(String fileName) {
         try {
-//            File file = new File("src/main/resources/" + fileName + ".wav");
             URL file = Sounds.class.getResource("/" + fileName + ".wav");
             Clip clip = AudioSystem.getClip();
-            // getAudioInputStream() also accepts a File or InputStream
             AudioInputStream ais = AudioSystem.getAudioInputStream(file);
             clip.open(ais);
             FloatControl gainControl =
                     (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-            gainControl.setValue(-12.0f); // change volume by x decibels (max. +6dB to increase, no limit on min)
+            gainControl.setValue(-12.0f);
             clip.loop(0);
         } catch (IOException | LineUnavailableException | UnsupportedAudioFileException ex)  {
             System.out.println("Valami nem stimmel a hanggal! :S");
+        }
+    }
+
+    public static void playBGsound(String fileName) {
+        try {
+            stopBGsound();
+            URL file = Sounds.class.getResource("/" + fileName + ".wav");
+            bgMusic = AudioSystem.getClip();
+            AudioInputStream ais = AudioSystem.getAudioInputStream(file);
+            bgMusic.open(ais);
+            FloatControl gainControl =
+                    (FloatControl) bgMusic.getControl(FloatControl.Type.MASTER_GAIN);
+            gainControl.setValue(-12.0f); // change volume by x decibels (max. +6dB to increase, no limit on min)
+            bgMusic.loop(-1);
+        } catch (IOException | LineUnavailableException | UnsupportedAudioFileException ex)  {
+            System.out.println("Valami nem stimmel a hanggal! :S");
+        }
+    }
+
+    public static void stopBGsound() {
+        if (bgMusic != null && bgMusic.isOpen() && bgMusic.isRunning()) {
+            bgMusic.stop();
+        }
+    }
+
+    public static void playEVSound(String fileName) {
+        try {
+            stopBGsound();
+            URL file = Sounds.class.getResource("/" + fileName + ".wav");
+            eventMusic = AudioSystem.getClip();
+            AudioInputStream ais = AudioSystem.getAudioInputStream(file);
+            eventMusic.open(ais);
+            FloatControl gainControl =
+                    (FloatControl) eventMusic.getControl(FloatControl.Type.MASTER_GAIN);
+            gainControl.setValue(-12.0f); // change volume by x decibels (max. +6dB to increase, no limit on min)
+            eventMusic.loop(Clip.LOOP_CONTINUOUSLY);
+        } catch (IOException | LineUnavailableException | UnsupportedAudioFileException ex)  {
+            System.out.println("Valami nem stimmel a hanggal! :S");
+        }
+    }
+
+    public static void stopEVsound() {
+        if (eventMusic != null && eventMusic.isOpen() && eventMusic.isRunning()) {
+            eventMusic.stop();
         }
     }
 }
